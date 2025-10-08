f858b9781776dd3d1c34bc5ca5ba0c42efd73860
Samuel Moelius <sam@moeli.us>
2025-10-08 08:53:20 -0400

    Add anchor-test-hack.sh

---
+++ anchor-test-hack.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+set -xeuo pipefail
+
+if [[ $# -ne 1 ]]; then
+    echo "$0: expect one argument: program name" >&2
+    exit 1
+fi
+
+PROGRAM="$1"
+
+PUBKEY="$(grep "^$PROGRAM = \"[^\"]*\"$" Anchor.toml | grep -o '\"[^"]*\"' | grep -o '[^"]*')"
+
+anchor build
+
+rm -rf test-ledger
+
+RUST_LOG=debug "solana-test-validator" \
+    "--ledger" \
+    "test-ledger" \
+    "--bpf-program" \
+    "$PUBKEY" \
+    "target/deploy/$PROGRAM.so" &
+
+anchor test --skip-deploy --skip-local-validator || true
+
+kill %%
